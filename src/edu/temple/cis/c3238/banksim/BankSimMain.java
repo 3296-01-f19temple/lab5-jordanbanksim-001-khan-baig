abb1321b8e0feba9b3a138b1a74bd8d6e484b090
Dawud Baig <dawudi284@gmail.com>
2019-10-12 16:35:03 -0400

    main

--- src/edu/temple/cis/c3238/banksim/BankSimMain.java
+++ src/edu/temple/cis/c3238/banksim/BankSimMain.java
@@ -2,27 +2,20 @@ package edu.temple.cis.c3238.banksim;
 
 
 public class BankSimMain {
+
     public static final int NACCOUNTS = 10;
     public static final int INITIAL_BALANCE = 10000;
 
     public static void main(String[] args) {
-        Bank bank = new Bank(NACCOUNTS, INITIAL_BALANCE);
+        Bank b = new Bank(NACCOUNTS, INITIAL_BALANCE);
         Thread[] threads = new Thread[NACCOUNTS];
-
         // Start a thread for each account
         for (int i = 0; i < NACCOUNTS; i++) {
-            threads[i] = new TransferThread(bank, i, INITIAL_BALANCE);
+            threads[i] = new TransferThread(b, i, INITIAL_BALANCE);
             threads[i].start();
         }
 
-        // Wait for all threads to finish
-        for (int i = 0; i < NACCOUNTS; i++) {
-            try {
-                threads[i].join();
-            } catch (InterruptedException ex) {
-                ex.printStackTrace();
-            }
-        }
-        bank.test();
+//        b.test();
+          System.out.printf("Bank transfer is in the process.\n");
     }
 }
